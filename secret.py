2102e9266f7accf56443e5a2cabc432619f1a6a4
Suyog Bhise <bhise.suyog@gmail.com>
2021-01-28 21:43:09 -0500

    Add files via upload

---
+++ secret.py
@@ -0,0 +1,8 @@
+def spotify_user_id():
+    return '5f6a3zxemz6ykg2mz53pb2su7'
+
+def spotify_token():
+    return 'BQBH7ypx1c3QxGWOnttC5O8E4jlI9S36jV7ARnEA7uKmz6gfz4xsWZYrmkZn7ZlZssfRQfZy_HpodLicMHlmRUYshSTYQz7Nr9IVwUmey-IhEgtdCFIGlDPzUs4oX8bvmwJxEULwRkT-tXfpAjZlnUYZp_xIyB5_cg9aFaytmebHwr9PGudDUIwKJrgFqU0'
+
+def last_fm_api_key():
+    return '3c815d4e147208e8f93d3bfabc62108e'
