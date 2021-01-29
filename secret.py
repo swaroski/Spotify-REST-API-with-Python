71f4e369924a029d2efcad9a207585b16f2d2fa9
Suyog Bhise <bhise.suyog@gmail.com>
2021-01-28 21:45:28 -0500

    Update secret.py
    Adding comments to the file

--- secret.py
+++ secret.py
@@ -1,8 +1,12 @@
 def spotify_user_id():
-    return '5f6a3zxemz6ykg2mz53pb2su7'
+    #Insert your User ID
+    return ''
 
 def spotify_token():
-    return 'BQBH7ypx1c3QxGWOnttC5O8E4jlI9S36jV7ARnEA7uKmz6gfz4xsWZYrmkZn7ZlZssfRQfZy_HpodLicMHlmRUYshSTYQz7Nr9IVwUmey-IhEgtdCFIGlDPzUs4oX8bvmwJxEULwRkT-tXfpAjZlnUYZp_xIyB5_cg9aFaytmebHwr9PGudDUIwKJrgFqU0'
+    #Insert your Spotify Token
+    #Henerate token here: https://developer.spotify.com/console/post-playlists
+    return ''
 
 def last_fm_api_key():
-    return '3c815d4e147208e8f93d3bfabc62108e'
+    #Insert your API key
+    return ''
